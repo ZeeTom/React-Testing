716635490a92d6f260362b30d4d950902f6cc60c
ZeeTom <zthom13@hotmail.com>
2021-08-10 16:33:50 -0600

    added smoketests and snapshots

--- react-state-carousel-coins/carousel/src/Card.js
+++ react-state-carousel-coins/carousel/src/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Card.css";
+import image1 from "./image1.jpg";
 
 function Card(props) {
   return (
@@ -12,5 +13,12 @@ function Card(props) {
     </div>
   );
 }
+
+Card.defaultProps = {
+  caption: "Test caption",
+  src: image1,
+  currNum: 1,
+  totalNum: 4,
+};
 
 export default Card;
--- react-state-carousel-coins/carousel/src/Carousel.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -2,12 +2,23 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
-it("works when you click on the right arrow", function() {
+it("renders without crashing", function () {
+  render(<Carousel />);
+});
+
+it("matches snapshot", function () {
+  const { container, debug } = render(<Carousel />);
+  expect(container).toMatchSnapshot();
+});
+
+it("works when you click on the right arrow", function () {
   const { container } = render(<Carousel />);
 
   // expect the first image to show, but not the second
   expect(
-    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+    container.querySelector(
+      'img[alt="Photo by Richard Pasquarella on Unsplash"]'
+    )
   ).toBeInTheDocument();
   expect(
     container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
@@ -19,7 +30,9 @@ it("works when you click on the right arrow", function() {
 
   // expect the second image to show, but not the first
   expect(
-    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+    container.querySelector(
+      'img[alt="Photo by Richard Pasquarella on Unsplash"]'
+    )
   ).not.toBeInTheDocument();
   expect(
     container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
